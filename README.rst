997e3d10409ef35227215767547e759a00318f1e
Horst Gutmann <zerok@zerokspot.com>
2008-10-06 22:11:10 +0200

    setup.py filled and README written. Ready for 0.1.0

---
+++ README.rst
@@ -0,0 +1,30 @@
+This simple recipe for zc.buildout fetches data from a given repository
+and stores it into its part's directory. A simple task using this 
+could look like this::
+    
+    [myapp]
+    recipe=zerokspot.recipe.git
+    repository=git://github.com/zerok/zerokspot.gitrecipe.git
+    rev=7c73978b55fcadbe2cd6f2abbefbedb5a85c2c8c
+
+This would store the repository under ${buildout:directory}/parts/myapp
+and keep it at exactly this revision, no matter what happens on the 
+server.
+
+The recipe has following options:
+
+repository
+    The absolute URL of the repository to be fetched
+
+rev
+    A revision/commit within this repository the environment
+    should use.
+
+branch
+    If you want to stay up to date with a certain branch other than
+    "master", use this. 
+
+newest
+    This overrides the newest-option of the global setting for this
+    part
+
--- setup.py
+++ setup.py
@@ -4,9 +4,21 @@ setup(
         name='zerokspot.recipe.git',
         author='Horst Gutmann',
         author_email='zerok@zerokspot.com',
-        version = '0.1',
-        install_requires = ['setuptools', 'zc.buildout'],
-        namespace_packages = ['zerokspot'],
-        packages = find_packages(exclude=['ez_setup']),
-        entry_points = {'zc.buildout': ['default = zerokspot.recipe.git:Recipe']}
+        description='Simple zc.buildout recipe for sources in a git repository',
+        version='0.1.0',
+        url='http://github.com/zerok/zerokspot.gitrecipe/',
+        install_requires=['setuptools', 'zc.buildout'],
+        namespace_packages=['zerokspot'],
+        packages=find_packages(exclude=['ez_setup']),
+        entry_points={'zc.buildout': ['default = zerokspot.recipe.git:Recipe']},
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Plugins',
+            'Framework :: Buildout',
+            'Intended Audience :: Developers',
+            'Programming Language :: Python',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: System :: Installation/Setup',
+            'License :: OSI Approved :: BSD License',
+            ]
         )
