96231a28fa14029b2a3bbc8c999817c6db1278bc
Horst Gutmann <zerok@zerokspot.com>
2009-06-16 14:04:39 +0200

    All fixed

--- zerokspot/recipe/git/__init__.py
+++ zerokspot/recipe/git/__init__.py
@@ -17,6 +17,9 @@ subversion repositories::
 This would store the cloned repository in ${buildout:directory}/parts/myapp.
 """
 
+import sys
+import os
+import functools
 import subprocess
 import os.path
 import zc.buildout
@@ -31,6 +34,8 @@ def get_reponame(url):
     if base.endswith('.git'):
         return base[:-4]
     return base
+
+execute = functools.partial(subprocess.call, shell=True, stdout=open(os.devnull, 'w'), stderr=subprocess.PIPE)
 
 class Recipe(object):
     """
@@ -54,22 +59,96 @@ class Recipe(object):
     """
     def __init__(self, buildout, name, options):
         self.buildout, self.name, self.options = buildout, name, options
+        self.cache_created = False
+        self.cache_updated = False
+        self.part_updated = False
+        self.cache_cloned = False
         self.installed_from_cache = None
         self.repository = options['repository']
         self.branch = options.get('branch', 'master')
         self.rev = options.get('rev', None)
         self.newest = options.get('newest',
                 buildout['buildout'].get('newest', "false")).lower() == 'true'
+        self.offline = options.get('offline', 'false').lower() == 'true'
         self.cache_install = options.get('install-from-cache', 
                 buildout['buildout'].get('install-from-cache', 'false')) \
                         .lower() == 'true'
         self.cache_name = options.get('cache-name', 
                 get_reponame(self.repository))
-        self.cache_path = os.path.join(buildout['buildout']['download-cache'],
-                self.cache_name)
+        self.download_cache = self.buildout['buildout'].get('download-cache', None)
+        if self.download_cache:
+            self.cache_path = os.path.join(buildout['buildout']['download-cache'],
+                    self.cache_name)
+        self.root_dir = self.buildout['buildout']['directory'] 
         options['location'] = os.path.join(
                 buildout['buildout']['parts-directory'], name)
         self.as_egg = options.get('as_egg', 'false').lower() == 'true'
+
+    def _clone(self, from_, to):
+        try:
+            status = execute('git clone "%s" "%s"' %
+                    (from_, to, ))
+            if status != 0:
+                raise zc.buildout.UserError("Couldn't clone %s into %s" % (
+                    from_, to, ))
+            os.chdir(to)
+            if self.branch != 'master':
+                branch = '-t "origin/%s"' %  (self.branch, )
+            else:
+                branch = '"master"'
+            status = execute(r'git checkout %s' % branch)
+            if status != 0:
+                raise zc.buildout.UserError('Failed to switch to branch "%s" '
+                    'in %s' % (self.branch, to, ))
+            if self.rev is not None:
+                status = execute(r'git checkout "%s"' % (self.rev,))
+            if status != 0:
+                raise zc.buildout.UserError("Failed to checkout revision")
+        finally:
+            os.chdir(self.root_dir)
+    
+    def _clone_cache(self):
+        """
+        Clone the cache into the parts directory.
+        """
+        if not os.path.exists(self.cache_path):
+            self._clone_upstream()
+        self._clone(self.cache_path, self.options['location'])
+        self.cache_cloned = True
+
+    def _clone_upstream(self):
+        """
+        Clone the upstream repository into the cache
+        """
+        self._clone(self.repository, self.cache_path)
+        self.cache_created = True
+
+    def _update_cache(self):
+        """
+        Updates the cached repository.
+        """
+        self._update_repository(self.cache_path)
+        self.cache_updated = True
+
+    def _update_part(self):
+        """
+        Updates the repository in the buildout's parts directory.
+        """
+        self._update_repository(self.options['location'])
+        self.part_updated = True
+
+    def _update_repository(self, path):
+        """
+        Update the repository from the given path
+        """
+        try:
+            os.chdir(path)
+            status = execute('git pull origin "%s"' % (self.branch))
+            if status != 0:
+                raise zc.buildout.UserError("Failed to pull")
+        finally:
+            os.chdir(self.root_dir)
+
 
     def install(self):
         """
@@ -82,47 +161,33 @@ class Recipe(object):
 
         copy_to_cache = False
         self.installed_from_cache = False
-        if self.cache_install:
-            # Check if a repo with the same name exists in the download cache
-            # and copy it into parts/<part-name>. If there is nothing inside
-            # the download cache, fetch the repo as usual and then copy it
-            # into the download cache.
+        if self.offline:
+            if not self.download_cache:
+                raise zc.buildout.UserError("Offline mode requested and no download-cache specified")
             if os.path.exists(self.cache_path):
-                shutil.copytree(self.cache_path, self.options['location']) 
-                os.chdir(self.buildout['buildout']['directory'])
+                # Just clone the cached repository
                 self.installed_from_cache = True
-                return self.options['location']
+                self._clone_cache()
             else:
-                copy_to_cache = True
-        status = subprocess.call(r'git clone "%s" "%s"' %
-                (self.repository, self.options['location']), shell=True)
-        if status != 0:
-            raise zc.buildout.UserError("Failed to clone repository")
-        try:
-            os.chdir(self.options['location'])
-            if self.branch != 'master':
-                branch = 'origin/%s' % (self.branch, )
+                raise zc.buildout.UserError("Offline mode requested but the "
+                        "requested repository doesn't exist in the cache")
+        else:
+            if self.download_cache:
+                if self.newest:
+                    # Update the cache before doing anything else
+                    if not os.path.exists(self.cache_path):
+                        self._clone_upstream()
+                    self._update_cache()
+                else:
+                    self.installed_from_cache = True
+                # Clone the cached repository
+                self._clone_cache()
             else:
-                branch = 'master'
-            status = subprocess.call(r'git checkout "%s"' %
-                    (branch,), shell=True)
-            if status != 0:
-                raise zc.buildout.UserError("Failed to switch branch")
-
-            if self.rev is not None:
-                status = subprocess.call(r'git checkout "%s"' % (self.rev,),
-                        shell=True)
-            if status != 0:
-                raise zc.buildout.UserError("Failed to checkout revision")
-
-            if self.as_egg:
-                self._install_as_egg()
-        finally:
-            if copy_to_cache:
-                shutil.copytree(self.options['location'], self.cache_path)
-                self.installed_from_cache = True
-            os.chdir(self.buildout['buildout']['directory'])
-            return self.options['location']
+                # Go the direct way since we have no cache
+                self._clone(self.repository, self.options['location'])
+        if self.as_egg:
+            self._install_as_egg()
+        return self.options['location']
 
     def update(self):
         """
@@ -134,16 +199,12 @@ class Recipe(object):
         if self.rev is None and self.newest:
             # Do an update of the current branch
             print "Pulling updates from origin"
+            if not self.cache_install:
+                self._update_cache()
+            self._update_part()
             os.chdir(self.options['location'])
-            try:
-                status = subprocess.call('git pull origin "%s"' % (self.branch),
-                        shell=True)
-                if status != 0:
-                    raise zc.buildout.UserError("Failed to pull")
-                if self.as_egg:
-                    self._install_as_egg()
-            finally:
-                os.chdir(self.buildout['buildout']['directory'])
+            if self.as_egg:
+                self._install_as_egg()
         else:
             # "newest" is also automatically disabled if "offline"
             # is set.
--- zerokspot/recipe/git/tests.py
+++ zerokspot/recipe/git/tests.py
@@ -1,6 +1,7 @@
 import unittest
 import sys
 import os
+import shutil
 import tempfile
 from zc.buildout import testing
 import zc.buildout
@@ -30,13 +31,23 @@ class UtilsTests(unittest.TestCase):
 class RecipeTests(unittest.TestCase):
     def setUp(self):
         self.tempdir = tempfile.mkdtemp()
+        # Prepare the cache
+        os.mkdir(os.path.join(self.tempdir, 'downloads'))
         self.tempcache = tempfile.mkdtemp()
         self.temprepo = os.path.join(self.tempcache, 'testrepo')
         os.mkdir(self.temprepo)
         testing.system('cd %s && git init' % self.temprepo)
         testing.write(self.temprepo, 'test.txt', 'TEST')
         testing.system('cd %s && git add test.txt && git commit -m "Init"' % self.temprepo)
+        testing.write(self.temprepo, 'test2.txt', 'TEST')
+        testing.system('cd %s && git checkout -b test && git add test2.txt && git commit -m "Test-branch" && git checkout master' % self.temprepo)
+        os.chdir(self.tempdir)
 
+    def _buildout(self):
+        build = zc.buildout.buildout.Buildout(os.path.join(self.tempdir, 'buildout.cfg'), [])
+        build.init(None)
+        build.install(None)
+        return build
     
     def testFetch(self):
         """
@@ -51,9 +62,73 @@ parts = gittest
 recipe = zerokspot.recipe.git
 repository = %(repo)s
         """ % {'repo' : self.temprepo})
-        testing.system('cd %s && buildout' % self.tempdir) 
+        self._buildout()
         self.assertTrue(os.path.exists(os.path.join(self.tempdir, 'parts', 'gittest', 'test.txt')))
     
+    def testCacheCreation(self):
+        """
+        Tests if a normal download also fills the cache.
+        """
+        testing.write(self.tempdir, 'buildout.cfg', """
+[buildout]
+parts = gittest
+download-cache = downloads
+
+[gittest]
+recipe = zerokspot.recipe.git
+repository = %(repo)s
+""" % {'repo' : self.temprepo })
+        self._buildout()
+        testing.ls(self.tempdir)
+        self.assertTrue(os.path.exists(os.path.join(self.tempdir, 'downloads', 'testrepo')))
+
+    def testFromCache(self):
+        """
+        Skip cache update on newest=="false"
+
+        If the buildout's newest setting is set to "false" and the cache is filled, 
+        the cache shouldn't receive an update.
+        """
+        shutil.copytree(self.temprepo, os.path.join(self.tempdir, 'downloads', 'test'))
+
+        testing.write(self.tempdir, 'buildout.cfg', """
+[buildout]
+parts = gittest
+download-cache = ./downloads
+install-from-cache = true
+newest = false
+
+[gittest]
+recipe = zerokspot.recipe.git
+repository = %(repo)s
+        """ % {'repo' : self.temprepo})
+        build = zc.buildout.buildout.Buildout(os.path.join(self.tempdir, 'buildout.cfg'), [])
+        build.install(None)
+        recipe = build['gittest'].recipe
+        self.assertFalse(recipe.cache_updated)
+        self.assertTrue(recipe.cache_cloned)
+
+    def testNonstandardBranch(self):
+        """
+        Tests if install from the dowload-cache works with a non-standard branch.
+        """
+        testing.write(self.tempdir, 'buildout.cfg', """
+[buildout]
+parts = gittest
+download-cache = %(cache)s
+install-from-cache = true
+offline = true
+
+[gittest]
+recipe = zerokspot.recipe.git
+branch = test
+repository = %(repo)s
+        """ % {'repo' : self.temprepo, 'cache': self.tempcache})
+        build = zc.buildout.buildout.Buildout(os.path.join(self.tempdir, 'buildout.cfg'), [])
+        build.install(None)
+        self.assertTrue(build['gittest'].recipe.installed_from_cache)
+
+
     def testOffline(self):
         """
         Tests if install from the dowload-cache works.
@@ -63,14 +138,29 @@ repository = %(repo)s
 parts = gittest
 download-cache = %(cache)s
 install-from-cache = true
+offline = true
 
 [gittest]
 recipe = zerokspot.recipe.git
 repository = %(repo)s
         """ % {'repo' : self.temprepo, 'cache': self.tempcache})
-        build = zc.buildout.buildout.Buildout(os.path.join(self.tempdir, 'buildout.cfg'), [])
-        build.install(None)
+        build = self._buildout()
         self.assertTrue(build['gittest'].recipe.installed_from_cache)
+
+    def testOfflineWithoutCache(self):
+        """
+        offline=='true' && no cache
+        """
+        testing.write(self.tempdir, 'buildout.cfg', """
+[buildout]
+parts = gittest
+offline = true
+
+[gittest]
+recipe = zerokspot.recipe.git
+repository = %(repo)s
+        """ % {'repo' : self.temprepo})
+        build = self._buildout()
 
     def tearDown(self):
         testing.rmdir(self.tempdir)
